b4cdd8057bad3680c7584876bd3b157097b06437
GBRBD <gaborattilabodi@gmail.com>
2019-12-05 22:23:00 +0100

    Using Nested Models & Providers, Implement toggle favorite button

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -13,10 +13,13 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        builder: (ctx) => products[i],
+        child: ProductItem(
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
