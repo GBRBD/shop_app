93f27101da6c90622da6feba3e7413c19d35a161
GBRBD <gaborattilabodi@gmail.com>
2019-12-05 21:51:12 +0100

    Show title in product details

--- lib/providers/products.provider.dart
+++ lib/providers/products.provider.dart
@@ -46,4 +46,8 @@ class Products with ChangeNotifier {
     // _items.add(value);
     notifyListeners();
   }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
 }
