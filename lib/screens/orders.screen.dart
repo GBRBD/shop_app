d653723a8fe8be45f7bd6e59d685a4543ebf5c1b
GBRBD <gaborattilabodi@gmail.com>
2019-12-12 18:55:21 +0100

    Create App Drawer

--- lib/screens/orders.screen.dart
+++ lib/screens/orders.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:shop_app/providers/orders.provider.dart' show Orders;
+import 'package:shop_app/widgets/app_drawer.widget.dart';
 import 'package:shop_app/widgets/order_item.widget.dart';
 
 class OrdersScreen extends StatelessWidget {
@@ -11,6 +12,7 @@ class OrdersScreen extends StatelessWidget {
     final orderData = Provider.of<Orders>(context);
     return Scaffold(
         appBar: AppBar(title: Text('Your Orders')),
+        drawer: AppDrawer(),
         body: ListView.builder(
           itemCount: orderData.orders.length,
           itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
--- lib/screens/products_overview.screen.dart
+++ lib/screens/products_overview.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.provider.dart';
 import 'package:shop_app/screens/cart.screen.dart';
+import 'package:shop_app/widgets/app_drawer.widget.dart';
 import 'package:shop_app/widgets/badge.widget.dart';
 
 import 'package:shop_app/widgets/products_grid.widget.dart';
@@ -61,6 +62,7 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
