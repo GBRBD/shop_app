fc764889bf76e66123847bbe7c0131fa6f3e9b96
GBRBD <gaborattilabodi@gmail.com>
2019-12-17 15:07:32 +0100

    Impllement delete produts functionality

--- lib/providers/products.provider.dart
+++ lib/providers/products.provider.dart
@@ -87,6 +87,11 @@ class Products with ChangeNotifier {
     }
   }
 
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
+    notifyListeners();
+  }
+
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
--- lib/widgets/product/user_product_item.widget.dart
+++ lib/widgets/product/user_product_item.widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.provider.dart';
 import 'package:shop_app/screens/edit_product.screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -31,7 +33,9 @@ class UserProductItem extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               color: Theme.of(context).primaryColor,
             )
           ],
