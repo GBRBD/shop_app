1a731ed0a8cd0987aeb42f545cb6fc5aa9b0fb90
GBRBD <gaborattilabodi@gmail.com>
2019-12-21 18:13:48 +0100

    Update edit_product.screen.dart

--- lib/screens/edit_product.screen.dart
+++ lib/screens/edit_product.screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 import 'package:shop_app/providers/product.provider.dart';
 import 'package:shop_app/providers/products.provider.dart';
 
