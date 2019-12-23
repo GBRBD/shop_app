571aac433a81184bc6d24d14695463d314ba8b38
GBRBD <gaborattilabodi@gmail.com>
2019-12-23 22:18:37 +0100

    Updating the "Favorite" Status Optimistically

--- lib/providers/product.provider.dart
+++ lib/providers/product.provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,25 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavStatus() {
+  Future<void> toggleFavStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
+    notifyListeners();
+    final url =
+        'https://flutter-shop-app-fee3a.firebaseio.com/products/$id.json';
+    try {
+      final response =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
+  }
+
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
     notifyListeners();
   }
 }
