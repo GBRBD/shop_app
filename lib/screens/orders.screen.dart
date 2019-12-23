e7422054041706c441b9119c0b7c972d0d9af0fa
GBRBD <gaborattilabodi@gmail.com>
2019-12-23 23:28:22 +0100

    Use FutureBuilder

--- lib/screens/orders.screen.dart
+++ lib/screens/orders.screen.dart
@@ -5,43 +5,32 @@ import 'package:shop_app/providers/orders.provider.dart' show Orders;
 import 'package:shop_app/widgets/app_drawer.widget.dart';
 import 'package:shop_app/widgets/order_item.widget.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
-        appBar: AppBar(title: Text('Your Orders')),
-        drawer: AppDrawer(),
-        body: _isLoading
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
+      appBar: AppBar(title: Text('Your Orders')),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (dataSnapshot.error != null) {
+            Text('error');
+          } else {
+            return Consumer<Orders>(
+              builder: (ctx, orderData, child) => ListView.builder(
                 itemCount: orderData.orders.length,
                 itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-              ));
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
