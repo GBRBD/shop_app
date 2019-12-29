a9899155b9ab0fe24cbfc57aaa530bfe2245f6ec
GBRBD <gaborattilabodi@gmail.com>
2019-12-29 20:37:33 +0100

    Implement firabse login

--- lib/providers/auth.provider.dart
+++ lib/providers/auth.provider.dart
@@ -8,9 +8,13 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDETn0OuXr2NnFx_RpeIZMUMZAXdvfEQCE';
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlSegment,
+  ) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDETn0OuXr2NnFx_RpeIZMUMZAXdvfEQCE';
     final response = await http.post(
       url,
       body: json.encode(
@@ -21,6 +25,23 @@ class Auth with ChangeNotifier {
         },
       ),
     );
+
     print(json.decode(response.body));
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(
+      email,
+      password,
+      'signUp',
+    );
+  }
+
+  Future<void> signIn(String email, String password) async {
+    return _authenticate(
+      email,
+      password,
+      'signInWithPassword',
+    );
   }
 }
