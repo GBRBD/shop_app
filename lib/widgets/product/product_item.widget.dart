a0f27b9c49fc61921a5fd46a8546cc43d6f6c9c4
GBRBD <gaborattilabodi@gmail.com>
2020-01-07 20:51:32 +0100

    Fading Loaded Images In (And Showing a Placeholder)

--- lib/widgets/product/product_item.widget.dart
+++ lib/widgets/product/product_item.widget.dart
@@ -29,10 +29,10 @@ class ProductItem extends StatelessWidget {
               arguments: product.id,
             );
           },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
+          child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
